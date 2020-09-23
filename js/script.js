1d895e664c81c288fb35249f6c158904f2cd668e
RyanzpLee <lzpryan@gmail.com>
2020-09-23 14:48:49 +0100

    Added updateRound function

--- js/script.js
+++ js/script.js
@@ -1,51 +1,34 @@
 // Create a global array called moves, which stores the 3 moves of rock paper scissors
 const moves = ['Rock', 'Paper', 'Scissors'];
-const wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector('.wrapper');
 const r = document.querySelector('.rock');
 const p = document.querySelector('.paper');
 const s = document.querySelector('.scissors');
 const text = document.querySelector('.result');
-const roundNumber = document.getElementById('roundNumber')
-const roundTotal = document.getElementById('roundTotal')
+const roundNumber = document.getElementById('roundNumber');
+const roundTotal = document.getElementById('roundTotal');
 const para = document.createElement('p');
-let round = 0
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 wrapper.addEventListener('click', game());
 roundNumber.textContent = `Round ${round}`;
 
 function game() {
-  let result;
-  round += 1
-
-  
-
-  r.addEventListener('click', () => {
-    result = playRound(moves[0], computerPlay());
-    score = updateScore(result);
-    roundTotal.textContent = `${score[0]} - ${score[1]}`
-    para.textContent = result;
-  });
-  p.addEventListener('click', () => {
-    result = playRound(moves[1], computerPlay());
-    score = updateScore(result);
-    roundTotal.textContent = `${score[0]} - ${score[1]}`;
-    para.textContent = result;
-  });
-  s.addEventListener('click', () => {
-    result = playRound(moves[2], computerPlay());
-    score = updateScore(result);
-    roundTotal.textContent = `${score[0]} - ${score[1]}`;
-    para.textContent = result;
-  });
-
-  
-  
-  text.appendChild(para);
-  console.log(result);
-
+  r.addEventListener('click', updateRound(moves[0]));
+  p.addEventListener('click', updateRound(moves[1]));
+  s.addEventListener('click', updateRound(moves[2]));
 }
 
-
+function updateRound(move) {
+  round += 1;
+  result = playRound(move, computerPlay());
+  score = updateScore(result);
+  roundTotal.textContent = `${score[0]} - ${score[1]}`;
+  para.textContent = result;
+  text.appendChild(para);
+}
 
 function computerPlay() {
   // create a random number between 0 - 2 and pick rock paper or scissors
@@ -69,57 +52,50 @@ function playRound(playerSelection, computerSelection) {
   // Compares the two variables
   // it's a tie
   if (playerSelection === computerSelection) {
-    result = "tie"
+    result = 'tie';
   }
 
   // rock beats scissors but loses to paper
   else if (playerSelection === moves[0]) {
     if (computerSelection === moves[2]) {
-      result = "win";
+      result = 'win';
     } else if (computerSelection === moves[1]) {
-      result = "lose";
+      result = 'lose';
     }
   }
 
   // paper beats rock, but loses to scissors
   else if (playerSelection === moves[1]) {
     if (computerSelection === moves[0]) {
-      result = "win";
+      result = 'win';
     } else if (computerSelection === moves[2]) {
-      result = "lose";
+      result = 'lose';
     }
   }
 
   //  scissors beats paper, but loses to rock
   else if (playerSelection === moves[2]) {
     if (computerSelection === moves[1]) {
-      result = "win";
+      result = 'win';
     } else if (computerSelection === moves[0]) {
-      result = "lose";
+      result = 'lose';
     }
   }
 
-  return result
+  return result;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-function updateScore(result){
-  switch (result){
-    
-    case "win":
+function updateScore(result) {
+  switch (result) {
+    case 'win':
       playerScore += 1;
-      return [playerScore, computerScore] 
+      return [playerScore, computerScore];
 
-    case "lose":
+    case 'lose':
       computerScore += 1;
       return [playerScore, computerScore];
 
-    case "tie":
+    case 'tie':
       return [playerScore, computerScore];
   }
-
-
-
 }
