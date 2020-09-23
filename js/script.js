ba1393f1c859a56c947578bf9aed32a9395315dd
RyanzpLee <lzpryan@gmail.com>
2020-09-23 16:29:28 +0100

    Fixed game function

--- js/script.js
+++ js/script.js
@@ -1,65 +1,73 @@
-// Create a global array called moves, which stores the 3 moves of rock paper scissors
 const moves = ['Rock', 'Paper', 'Scissors'];
-const wrapper = document.querySelector('.wrapper');
-const r = document.querySelector('.rock');
-const p = document.querySelector('.paper');
-const s = document.querySelector('.scissors');
-const text = document.querySelector('.result');
 const roundNumber = document.getElementById('roundNumber');
 const roundTotal = document.getElementById('roundTotal');
 const para = document.createElement('p');
+let gameButtons = document.getElementById('gameplay-buttons');
+let playAgain = document.getElementById('play-again');
+let results = document.getElementById('results');
 let round = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-wrapper.addEventListener('click', game());
 roundNumber.textContent = `Round ${round}`;
 
-function game() {
-  r.addEventListener('click', updateRound(moves[0]));
-  p.addEventListener('click', updateRound(moves[1]));
-  s.addEventListener('click', updateRound(moves[2]));
-}
+const r = document.querySelector('.rock');
+r.onclick = () => {
+  playerSelection = moves[0];
+  document.getElementById('player-choice').innerHTML = `Your choice: ${playerSelection}`;
+  playRound(playerSelection, computerPlay());
+};
 
-function updateRound(move) {
-  round += 1;
-  result = playRound(move, computerPlay());
-  score = updateScore(result);
-  roundTotal.textContent = `${score[0]} - ${score[1]}`;
-  para.textContent = result;
-  text.appendChild(para);
-}
+const p = document.querySelector('.paper');
+p.onclick = () => {
+  playerSelection = moves[1];
+  document.getElementById('player-choice').innerHTML = `Your choice: ${playerSelection}`;
+  playRound(playerSelection, computerPlay());
+};
+const s = document.querySelector('.scissors');
+s.onclick = () => {
+  playerSelection = moves[2];
+  document.getElementById('player-choice').innerHTML = `Your choice: ${playerSelection}`;
+  playRound(playerSelection, computerPlay());
+};
+
+playAgain.addEventListener('click', () => {
+  window.location.reload();
+});
 
 function computerPlay() {
-  // create a random number between 0 - 2 and pick rock paper or scissors
+  let computerChoice = document.getElementById('computer-choice');
   let index = Math.floor(Math.random() * 3);
-
-  // store this index in the variable computerMove
   let computerMove = moves[index];
-
-  //  Return the computer's random move
+  computerChoice.innerHTML = `Computer's choice: ${computerMove}`;
   return computerMove;
 }
 
 // create a function that takes two command line arguments playerSeleciton and computerSelection
 function playRound(playerSelection, computerSelection) {
   // output the message "You lose/ You win! X beats Y" depending on the move comparison
-  const win = `You Win! ${playerSelection} beats ${computerSelection}`;
-  const lose = `You Lose! ${playerSelection} loses to ${computerSelection}`;
-  const tie = `You Tie! ${playerSelection} ties with ${computerSelection}`;
   let result;
+  round++;
+  roundNumber.innerHTML = `Round ${round}`;
 
   // Compares the two variables
   // it's a tie
   if (playerSelection === computerSelection) {
+    results.innerHTML = "It's a draw, try again.";
     result = 'tie';
   }
 
   // rock beats scissors but loses to paper
   else if (playerSelection === moves[0]) {
     if (computerSelection === moves[2]) {
+      playerScore += 1;
+      results.innerHTML = 'You win this round!';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'win';
     } else if (computerSelection === moves[1]) {
+      computerScore += 1;
+      results.innerHTML = 'You lose this round.';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'lose';
     }
   }
@@ -67,8 +75,14 @@ function playRound(playerSelection, computerSelection) {
   // paper beats rock, but loses to scissors
   else if (playerSelection === moves[1]) {
     if (computerSelection === moves[0]) {
+      playerScore += 1;
+      results.innerHTML = 'You win this round!';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'win';
     } else if (computerSelection === moves[2]) {
+      computerScore += 1;
+      results.innerHTML = 'You lose this round.';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'lose';
     }
   }
@@ -76,26 +90,34 @@ function playRound(playerSelection, computerSelection) {
   //  scissors beats paper, but loses to rock
   else if (playerSelection === moves[2]) {
     if (computerSelection === moves[1]) {
+      playerScore += 1;
+      results.innerHTML = 'You win this round!';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'win';
     } else if (computerSelection === moves[0]) {
+      computerScore += 1;
+      results.innerHTML = 'You lose this round.';
+      roundTotal.innerHTML = `${playerScore} - ${computerScore}`;
       result = 'lose';
     }
   }
-
+  isMatchOver();
   return result;
 }
 
-function updateScore(result) {
-  switch (result) {
-    case 'win':
-      playerScore += 1;
-      return [playerScore, computerScore];
-
-    case 'lose':
-      computerScore += 1;
-      return [playerScore, computerScore];
-
-    case 'tie':
-      return [playerScore, computerScore];
+function isMatchOver() {
+  if (computerScore > 4 || playerScore > 4) {
+    gameButtons.style.display = 'none';
+    setTimeout(function () {
+      playAgain.style.display = 'block';
+    }, 200);
+    if (computerScore === 5) {
+      results.innerHTML = 'Sorry, you lost the game.';
+    } else if (playerScore === 5) {
+      results.innerHTML = 'Congratulations, you win the game';
+    } else {
+      return;
+    }
   }
+  return;
 }
